459473bce2ce0a7ceea0fd50070af510392739a0
Daniel Paradis <dan.paradis@maine.edu>
2018-09-14 11:44:07 -0400

    Added cost functionality, sigval still broken

--- settest.py
+++ settest.py
@@ -6,6 +6,7 @@ import random
 
 def set_cover(universe, subsets, num_subsets, cost, sigval, sigstr, n):
 	"""Find a family of subsets that covers the universal set"""
+	total_cost = 0;
 	overlap = set();
 	elements = set(e for s in subsets for e in s)
 
@@ -21,11 +22,16 @@ def set_cover(universe, subsets, num_subsets, cost, sigval, sigstr, n):
 	for i in range(num_subsets):
 		if covered == elements:
 			break;
-		max_importance = calc_importance(subsets, covered.difference(universe),
+		max_importance = calc_importance(subsets, covered,
 									cost, sigval, sigstr, n);
 #		subset = max(subsets, key=lambda s: len(s - covered));
-		subset = subsets[max_importance]
+		subset = subsets[max_importance];
+		total_cost += int(cost[max_importance]);
+
 		subsets.remove(subsets[max_importance]);
+		cost.remove(cost[max_importance]);
+#		sigval.remove(sigval[max_importance]);
+
 		cover.append(subset);
 		covered |= subset;
 
@@ -33,30 +39,40 @@ def set_cover(universe, subsets, num_subsets, cost, sigval, sigstr, n):
 			overlap = subset.intersection(covered);
 
 	if elements == covered:
- 		return cover, overlap, 1;
+ 		return cover, overlap, 1, total_cost;
 	else:
-		return universe.difference(covered), overlap, 0;
+		return universe.difference(covered), overlap, 0, total_cost;
 
-def calc_importance(subsets, uncovered, cost, sigval, sigstr, n):
-	importance = [0] * n * n;
-	index = 0;
+def calc_importance(subsets, covered, cost, sigval, sigstr, n):
+	importance = 0;
 	max_importance = 0;
+	importance_index = 0;
+	index = 0;
+	sigval_val = 0;
+
+	# TODO add sigstr to weighting.  Need to sum all elements in each set
+	# and return an array of sums
 
 	for x in subsets:
-		new_covers = x.intersection(uncovered);
-		if len(new_covers) == 0:
-			index += 1;
-			continue;
 
-		cover_val = int((len(new_covers)/len(uncovered))*100);
-		importance[index] = cover_val + cost[index] + sigval[index] + sigstr[index];
+		for z in (x - covered):
+			sigval_val += int(sigval[z]);
 
-		if importance[index] > max_importance:
-			max_importance = index;
+		cover_val = len(x - covered)*n;
+		cost_val = n*n - int(cost[index]);
+
+		importance = cover_val + cost_val + sigval_val;
+
+		if importance > max_importance:
+			max_importance = importance;
+			importance_index = index;
 
 		index += 1;
 
-	return max_importance;
+	for x in subsets[max_importance]:
+		sigval[x] = 0;
+
+	return importance_index;
  
 def get_sets_from_file(filename):
 	fp = open(filename, "r");
@@ -82,7 +98,7 @@ def get_sets_from_file(filename):
 
 	return subsets, n;
 
-def plot_heatmap(cover, overlap, covered, n):
+def plot_coverage(cover, overlap, covered, n):
 	data = [[1]*n for _ in range(n)];
 
 	if covered == 0:
@@ -112,6 +128,11 @@ def choose_subsets(num_subsets, s, n):
 def main():
 	best_cover = 0;
 	covered = 0;
+	total_value = 0;
+	total_cost = 0;
+	cover_value = 0;
+	possible_value = 0;
+	potential_cost = 0;
 	num_subsets = int(sys.argv[1]);
 
 	s, n = get_sets_from_file("covered.csv");
@@ -119,8 +140,12 @@ def main():
 	sigval, n = get_sets_from_file("sigval.csv");
 	cost, n = get_sets_from_file("cost.csv");
 
+	for i in range(n*n):
+		possible_value += int(sigval[i]);
+		potential_cost += int(cost[i]);
+
 	universe = set(range(0, n*n));
-	cover, overlap, covered = set_cover(universe, s, num_subsets, 
+	cover, overlap, covered, total_cost = set_cover(universe, s, num_subsets, 
 										cost, sigval, sigstr, n);
 
 #	while covered == 0:
@@ -132,8 +157,17 @@ def main():
 
 #		if len(cover) > best_cover:
 #			best_cover = len(cover);
+	for x in cover:
+		total_cost
+	cost_efficiency = str(1 - float(total_cost)/float(potential_cost)) + "%";
 
 	if covered:
+		print "Cost";
+		print total_cost;
+		print "Potential Cost";
+		print potential_cost;
+		print "Cost Effeciency";
+		print cost_efficiency;
 		print "Overlap";
 		print list(overlap);
 
@@ -142,12 +176,23 @@ def main():
 		for x in cover:
 			print list(x);
 	else:
+		print "Cost";
+		print total_cost;
+		print "Potential Cost";
+		print potential_cost;
+		print "Cost Effeciency";
+		print cost_efficiency;
+#		print "Value Covered";
+#
+#		print str(cover_value);
+#		print "Total possible value";
+#		print str(total_value);
 		print "Uncovered";
 		print list(cover);
 
 		print "Overlap";
 		print list(overlap);
 
-	plot_heatmap(cover, overlap, covered, n);
+	plot_coverage(cover, overlap, covered, n);
 
 main();
